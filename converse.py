827de251a4a896549badaf581f2c0ab1bb787555
Emmett Butler <emmett.butler321@gmail.com>
2013-06-30 23:30:59 -0400

    bit more cleaning

--- converse.py
+++ converse.py
@@ -8,9 +8,6 @@ import constants
 if __name__ == "__main__":
     converse = Shell()
 
-    main_menu = Menu('main')
-    main_menu.title = "Converse. It's a thing"
-
     new_com = Command('new <topic>', 'Create a new topic')
     def _run(tokens):
         converse.put("new topic %s" % tokens[1])
@@ -36,8 +33,6 @@ if __name__ == "__main__":
         return constants.CHOICE_QUIT
     quit_com.set_run_function(_run)
 
-    edit_menu = Menu('edit')
-    edit_menu.title = "Editing menu"
 
     test_com = Command('test', 'test that this works')
     def _run(tokens):
@@ -45,8 +40,14 @@ if __name__ == "__main__":
         return constants.CHOICE_LIST
     test_com.set_run_function(_run)
 
+    main_menu = Menu('main')
+    main_menu.title = "Converse. It's a thing"
     main_menu.commands = [new_com, load_com, list_com, quit_com]
+
+    edit_menu = Menu('edit')
+    edit_menu.title = "Editing menu"
     edit_menu.commands = [test_com, quit_com]
+
     converse.menus = [main_menu, edit_menu]
 
     converse.main_loop()
